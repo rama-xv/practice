7d63327f495d59d8fa4c98c4252b1b81e2f04ee1
Rama Ibseileh <ramaib@umich.edu>
2025-07-02 12:18:43 -0400

    Initial commit with quraticsolver.java

---
+++ quraticsolver.java
@@ -0,0 +1,22 @@
+
+public class quraticsolver {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = -3;
+        int c = 2;
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant > 0) {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            System.out.println("Two real roots: " + root1 + " and " + root2);
+        } else if (discriminant == 0) {
+            double root = -b / (2.0 * a);
+            System.out.println("One real root: " + root);
+        } else {
+            System.out.println("No real roots. Discriminant is negative.");
+        }
+    }
+}
+
